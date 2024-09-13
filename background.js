128ec231b3bdf9fab3282b33e398c4ff02eee2e5
namankumar9 <71693531+namankumar9@users.noreply.github.com>
2024-09-13 23:30:18 +0530

    All working fine 3

--- background.js
+++ background.js
@@ -1,16 +1,1 @@
-chrome.webNavigation.onBeforeNavigate.addListener((details) => {
-  chrome.storage.local.get(['texts'], (result) => {
-    const urls = result.texts || [];
-    const currentUrl = new URL(details.url).hostname;
 
-    // Check if any saved URL matches the current URL
-    const shouldBlock = urls.some(url => currentUrl.includes(url));
-
-    if (shouldBlock) {
-      chrome.scripting.executeScript({
-        target: { tabId: details.tabId },
-        files: ['content.js']
-      });
-    }
-  });
-}, {url: [{urlMatches: '.*'}]});
